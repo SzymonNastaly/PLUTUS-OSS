855c613c90a97255199e12823ff7cb186b1be88e
Szymon Nastaly <szymonnastaly@gmail.com>
2018-09-26 21:19:12 +0200

    Added ViewSet for Stock Model

--- PlutusOSS/data/views.py
+++ PlutusOSS/data/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from data.models import Stock
+from data.serializers import StockSerializer
 
-# Create your views here.
+class StockViewSet(viewsets.ModelViewSet):
+    """ViewSet that represents Stock model"""
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+
