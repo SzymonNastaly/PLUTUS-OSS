733652816b8613f416b6665637c5318f4a986f91
Szymon Nastaly <szymonnastaly@gmail.com>
2018-09-28 21:55:08 +0200

    Add specific serializer for list of stocks

--- PlutusOSS/data/views.py
+++ PlutusOSS/data/views.py
@@ -1,9 +1,18 @@
 from rest_framework import viewsets
 from data.models import Stock
 from data.serializers import StockSerializer
+from data.serializers import StockListSerializer
 
 class StockViewSet(viewsets.ModelViewSet):
     """ViewSet that represents Stock model"""
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
+    list_serializer_class = StockListSerializer
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            if hasattr(self, 'list_serializer_class'):
+                return self.list_serializer_class
+
+        return super(StockViewSet, self).get_serializer_class()
 
