9e57c81220d2a52ecadb9e6b0fc0f4343b74fc38
Szymon Nastaly <szymonnastaly@gmail.com>
2018-10-13 10:18:08 +0200

    Added Token Authentication & obtain_auth_token function

--- PlutusOSS/PlutusOSS/settings.py
+++ PlutusOSS/PlutusOSS/settings.py
@@ -30,8 +30,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'data'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    )
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
