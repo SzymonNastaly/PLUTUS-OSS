04a181c88054905102f3258df4b30db9b97a67d4
Szymon Nastaly <szymonnastaly@gmail.com>
2018-12-26 10:58:26 +0100

    Added a info/index (basic information) and a signup page

--- PlutusOSS/PlutusOSS/urls.py
+++ PlutusOSS/PlutusOSS/urls.py
@@ -6,31 +6,16 @@ from data import views
 from rest_framework.authtoken import views as tokenviews
 from django.contrib import admin
 
-"""PlutusOSS URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-
 router = routers.DefaultRouter()
 router.register(r'stock', views.StockViewSet)
 
 
 urlpatterns = [
-    url(r'^', include(router.urls)),
+    url(r'^api/', include(router.urls), name="api"),
     path('admin/', admin.site.urls),
     url(r'^api-token-auth/', tokenviews.obtain_auth_token), #returns token for account, when correct login
     url(r'^api-auth/', include('rest_framework.urls')), #Login button
+    url(r'^signup/', views.signup),
+    url(r'^', views.index),
 ]
 
